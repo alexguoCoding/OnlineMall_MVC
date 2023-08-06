50758c4d23db56b6b7b53ee892d19ad99fcf6152
songkunGuo <songkun.guo01@gmail.com>
2023-08-06 13:03:29 -0600

    small fix

--- BulkyWeb/wwwroot/js/company.js
+++ BulkyWeb/wwwroot/js/company.js
@@ -29,7 +29,7 @@ function loadDataTable() {
 
 function Delete(url) {
     Swal.fire({
-        title: 'Are you sure?',
+        title: 'Are you OK?',
         text: "You won't be able to revert this!",
         icon: 'warning',
         showCancelButton: true,
