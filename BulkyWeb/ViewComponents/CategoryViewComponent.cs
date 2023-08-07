e1e4ab81c6ce234f64aa890372ce795022dc644f
songkunGuo <songkun.guo01@gmail.com>
2023-08-06 21:26:23 -0600

    finish view each category product

---
+++ BulkyWeb/ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BulkyBookWeb.ViewComponents
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+          
+                return View(_unitOfWork.Category.GetAll());
+
+        }
+
+    }
+}
